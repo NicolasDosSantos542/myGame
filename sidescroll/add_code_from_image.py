ebb88ca2128c987ed8f4bfecb61ccc83828ec8ac
Nico <nicolas.de-meyere-dos-santos@epitech.eu>
2022-09-16 13:58:22 +0200

    script ok (with print), changes in images folder

--- sidescroll/add_code_from_image.py
+++ sidescroll/add_code_from_image.py
@@ -4,10 +4,10 @@ print(os.getcwd())
 from PIL import Image
 import glob
 
-root_path = 'sidescroll/game/assets/onix'
-def create_image_list():
+root_path = 'sidescroll/game/assets'
+def create_image_list(path):
     image_list = []
-    for filename in glob.glob(root_path+'/*.png'): #assuming gif
+    for filename in glob.glob(path+'/*.png'): #assuming gif
         im=Image.open(filename)
 
         image_list.append({
@@ -15,28 +15,34 @@ def create_image_list():
             "path" : filename,
             "width" : im.width,
             "height" : im.height })
-    print(image_list)
     return image_list
 
 
 def find_image_name_from_path(path):
     return os.path.basename(path)
-    
 
-def create_phaser_loader_from_names(dict, image_or_sprite = ""):
+
+def create_phaser_loader_from_names(dict):
     for element in dict:
         name = os.path.splitext(element["name"])[0]
-        if image_or_sprite == "image":
+        if "image" in element['path']:
             loader_string = f"game.load.image('{name}', '{ element['path'] }') ;"
-        elif image_or_sprite == "sprite":
+        elif "sprite" in element['path']:
             loader_string = f"game.load.spritesheet('{name}', '{ element['path'] }', \u007b frameWidth: 50, frameHeight: {element['height']}, startFrame:0, endFrame:2\u007d) ;"
         else:
-            loader_string = "no parameter or bad string in parameter"
-        # print(type(element))
+            loader_string = "bad folder"
         print(loader_string)
 
+def parse_folder_and_create_list(folder):
+    for root, dirs, files in os.walk(folder):
+        # find_folder_name(root)
+        print(f"//{root}")
+        list= create_image_list(root)
+        create_phaser_loader_from_names(list)
 
 
-list = create_image_list()
-print (create_phaser_loader_from_names(list, "sprite"))
 
+
+# list = create_image_list()
+# print (create_phaser_loader_from_names(list, "sprite"))
+parse_folder_and_create_list(root_path)
