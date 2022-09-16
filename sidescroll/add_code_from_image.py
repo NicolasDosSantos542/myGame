da102ddf47b6c423140e98a814e80067bcfbf9c0
Nico <nicolas.de-meyere-dos-santos@epitech.eu>
2022-09-16 12:16:31 +0200

    add custom height

--- sidescroll/add_code_from_image.py
+++ sidescroll/add_code_from_image.py
@@ -8,26 +8,35 @@ root_path = 'sidescroll/game/assets/onix'
 def create_image_list():
     image_list = []
     for filename in glob.glob(root_path+'/*.png'): #assuming gif
-        # im=Image.open(filename)
+        im=Image.open(filename)
 
-        image_list.append({"name" :find_image_name_from_path(filename), "path" : filename})
+        image_list.append({
+            "name" :find_image_name_from_path(filename),
+            "path" : filename,
+            "width" : im.width,
+            "height" : im.height })
     print(image_list)
     return image_list
-# for root, dirs, files in os.walk('/sidescroll/assets'):
-#     print('toto')
-#     print(root, dirs, files)
+
 
 def find_image_name_from_path(path):
     return os.path.basename(path)
+    
 
-def create_phaser_loader_from_names(dict):
+def create_phaser_loader_from_names(dict, image_or_sprite = ""):
     for element in dict:
-        # print(type(element))
         name = os.path.splitext(element["name"])[0]
-        loader_string = f"    game.load.image('{name}', '{ element['path'] }', \u007b frameWidth: 50, frameHeight: 50, startFrame:0, endFrame:2\u007d) ;"
+        if image_or_sprite == "image":
+            loader_string = f"game.load.image('{name}', '{ element['path'] }') ;"
+        elif image_or_sprite == "sprite":
+            loader_string = f"game.load.spritesheet('{name}', '{ element['path'] }', \u007b frameWidth: 50, frameHeight: {element['height']}, startFrame:0, endFrame:2\u007d) ;"
+        else:
+            loader_string = "no parameter or bad string in parameter"
+        # print(type(element))
         print(loader_string)
 
 
+
 list = create_image_list()
-print (create_phaser_loader_from_names(list))
+print (create_phaser_loader_from_names(list, "sprite"))
 
